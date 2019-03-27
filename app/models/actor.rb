611421883526c113ad723e00dbb8ed132ce62a0d
Patrick Kim <pkim050@gmail.com>
2019-03-27 09:21:37 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    name = "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    arr = []
+    char_names = self.characters.collect {|element| element.name}
+    show_names = self.shows.collect {|element2| element2.name}
+    #binding.pry
+    char_show = char_names + show_names
+    arr << char_show.join(" - ")
+    arr
+  end
 end
